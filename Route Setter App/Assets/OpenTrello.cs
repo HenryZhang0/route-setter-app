ba1e02c6ac6cd3dd17ed65579054322d2d18ba18
Henry Zhang <zhangyiheng2003@gmail.com>
2022-11-12 17:55:03 -0500

    Trello Link

---
+++ Route Setter App/Assets/OpenTrello.cs
@@ -0,0 +1,22 @@
+ using UnityEngine;
+ using System.Collections;
+ 
+ public class OpenTrello : MonoBehaviour 
+ {
+     Ray ray;
+     RaycastHit hit;
+     
+     void Update()
+     {
+         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+         if(Physics.Raycast(ray, out hit))
+         {
+             if(Input.GetMouseButtonUp(0)) {
+                 print(hit.collider.name);
+                 if(hit.collider.name == "Cube") {
+                    Application.OpenURL("https://trello.com/b/CHA5xMqr/climbing-app");
+                 }
+             }
+         }
+     }
+ }

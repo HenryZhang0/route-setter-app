987bec222f79a80514deb1717c4890cfcddcfd2d
Henry Zhang <zhangyiheng2003@gmail.com>
2022-11-14 17:31:50 -0500

    Camera stuff and basic map editor

--- Route Setter App/Assets/Scripts/TouchInput.cs
+++ Route Setter App/Assets/Scripts/TouchInput.cs
@@ -2,31 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-
 public class TouchInput : MonoBehaviour
 {
 #if UNITY_IOS || UNITY_ANDROID
     public Camera Camera;
     public GameObject target;
+    public GameObject DefaultPosition;
     public GameObject p;
 
-    protected Plane Plane;
+    protected Plane Plane; // camera plane
     public bool Rotate;
 
-    private void Awake()
-    {
-        if (Camera == null)
-            Camera = Camera.main;
-    }
+    public float rotationSensitivity = 2.5f;
+    public float scrollSensitivity = 1f;
+    public float zoomSensitivity = 1f;
+    Vector2 orbitAngles = new Vector2(45f, 0f);
 
     private void Update()
     {
-
         //Update Plane
         if (Input.touchCount >= 1){
             // Vector3 normal = (target.transform.position - Camera.transform.position).normalized;
@@ -39,11 +32,20 @@ public class TouchInput : MonoBehaviour
             p.transform.Rotate(-90,0,0);
         }
 
+        if (Input.touchCount == 1)
+        {
+            Vector2 input = Input.GetTouch(0).deltaPosition;
+            input = new Vector2(input.y*-1, input.x);
+            input *= rotationSensitivity;
+            // OrbitCamera(input);
+        }
+
+
         var Delta1 = Vector3.zero;
         var Delta2 = Vector3.zero;
 
         //Scroll
-        if (Input.touchCount >= 1)
+        if (Input.touchCount >= 2)
         {
             Delta1 = PlanePositionDelta(Input.GetTouch(0));
             if (Input.GetTouch(0).phase == TouchPhase.Moved){
@@ -105,54 +107,29 @@ public class TouchInput : MonoBehaviour
         return Vector3.zero;
     }
 
-    private void OnDrawGizmos()
+    public void OrbitCamera(Vector2 input)
     {
-        Gizmos.DrawLine(transform.position, transform.position + transform.up);
+        Vector3 focusPoint = target.transform.position;
+        const float e = 0.001f;
+
+        Vector2 cameraAngle = Camera.transform.eulerAngles; 
+		if (input.x < -e || input.x > e || input.y < -e || input.y > e) {
+			orbitAngles += rotationSensitivity * Time.unscaledDeltaTime * input;
+            cameraAngle += rotationSensitivity * Time.unscaledDeltaTime * input;
+		}
+        
+        Quaternion lookRotation = Quaternion.Euler(orbitAngles);
+		Vector3 lookDirection = lookRotation * Vector3.forward * Vector3.Distance(target.transform.position, Camera.transform.position);
+		Vector3 lookPosition = focusPoint - lookDirection;
+        // Camera.transform.SetPositionAndRotation(lookPosition, Quaternion.Euler(cameraAngle));
+        Vector3 rotateAmount = new Vector3(input.x, input.y, 0.0f);
+        // Camera.transform.rotation = Quaternion.Euler(Camera.transform.eulerAngles + rotationSensitivity * Time.unscaledDeltaTime * rotateAmount);
+        Camera.transform.position = lookPosition;
+    }
+
+    public void ResetCamera()
+    {
+        Camera.transform.SetPositionAndRotation(DefaultPosition.transform.position, Quaternion.Euler(DefaultPosition.transform.eulerAngles));
     }
 #endif
 }
-
-// {
-//     public Camera camera;
-//     public GameObject target;
-//     public float rotationSpeed = 1f;
-//     Vector3 focusPoint;
-//     Vector2 orbitAngles = new Vector2(45f, 0f);
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-        
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         focusPoint = target.transform.position;
-//         OrbitControls();
-
-//     }
-
-
-//     private void OrbitControls()
-//     {
-//         if(Input.touchCount != 1) return;
-
-//         Vector2 input = Input.GetTouch(0).deltaPosition;
-//         input = new Vector2(input.y*-1, input.x);
-// 		input *= rotationSpeed;
-// 		OrbitCamera(input);
-//     }
-
-//     public void OrbitCamera(Vector2 input)
-//     {
-//         const float e = 0.001f;
-// 		if (input.x < -e || input.x > e || input.y < -e || input.y > e) {
-// 			orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
-// 		}
-        
-//         Quaternion lookRotation = Quaternion.Euler(orbitAngles);
-// 		Vector3 lookDirection = lookRotation * Vector3.forward;
-// 		Vector3 lookPosition = focusPoint - lookDirection;
-//         camera.transform.SetPositionAndRotation(lookPosition, lookRotation);
-//     }
-// }
